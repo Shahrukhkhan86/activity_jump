ee55422f5b32927dd6c5c843f3a2d3c1f8486462
Shahrukhkhan86 <shahrukh.khan_mca18@gla.ac.in>
2019-02-20 23:53:09 +0530

    init all commit

---
+++ app/src/main/java/com/example/a4feb3/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.a4feb3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn =findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
+
+
+    }
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Toast.makeText(this, "Onstart", Toast.LENGTH_SHORT).show();
+    }
+}
